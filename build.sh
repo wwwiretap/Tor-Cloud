485448016f3e58fa4a28a482afb8640b4cbf13de
inf0 <sina@anarchy.cx>
2011-07-04 16:37:04 -0700

    fixed error with ubuntu image url
    added new AMI ID to index.html

--- build.sh
+++ build.sh
@@ -10,18 +10,20 @@ host=$(ec2-describe-instances $iid | awk '-F\t' '$2 == iid { print $4 }' iid=${i
 echo ${host}
 
 # create and attached ebs volume to be used for snapshot
-ec2-create-volume --size 8 --availability-zone ${zone}
-vol=$(ec2-create-volume --size 8 --availability-zone ${zone} | awk {'print $2'})
+ec2-create-volume --size 4 --availability-zone ${zone}
+vol=$(ec2-create-volume --size 4 --availability-zone ${zone} | awk {'print $2'})
 ec2-attach-volume --instance ${iid} --device /dev/sdh ${vol}
 
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/inf0.pem ubuntu@${host} -q -v -t "sudo chown ubuntu:ubuntu /mnt && cd /mnt && sudo wget http://uec-images.ubuntu.com/releases/10.04/release/ubuntu-10.04-server-uec-i386.tar.gz -O ubuntu-10.04-server-uec-i386.tar.gz && sudo tar -Sxvzf /mnt/ubuntu-10.04-server-uec-i386.tar.gz && mkdir src target && sudo mount -o loop,ro /mnt/lucid-server-uec-i386.img /mnt/src && sudo wget https://github.com/inf0/Tor-Cloud/raw/master/ioerror.sh -O /mnt/src/etc/ioerror.sh && sudo wget https://github.com/inf0/Tor-Cloud/raw/master/rc.local -O /mnt/src/etc/rc.local && sudo chmod +x /mnt/src/etc/ioerror.sh && sudo chmod +x /mnt/src/etc/rc.local && sudo mkfs.ext4 -F -L uec-rootfs /dev/sdh && sudo mount /dev/sdh /mnt/target && sudo rsync -aXHAS /mnt/src/ /mnt/target && sudo umount /mnt/target && sudo umount /mnt/src"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/inf0.pem ubuntu@${host} -q -v -t "sudo chown ubuntu:ubuntu /mnt && cd /mnt && sudo wget http://uec-images.ubuntu.com/server/lucid/current/lucid-server-uec-i386.tar.gz -O lucid-server-uec-i386.tar.gz && sudo tar -Sxvzf /mnt/lucid-server-uec-i386.tar.gz && mkdir src target && sudo mount -o loop,rw /mnt/lucid-server-uec-i386.img /mnt/src && sudo wget https://github.com/inf0/Tor-Cloud/raw/master/ioerror.sh -O /mnt/src/etc/ioerror.sh && sudo wget https://github.com/inf0/Tor-Cloud/raw/master/rc.local -O /mnt/src/etc/rc.local && sudo chmod +x /mnt/src/etc/ioerror.sh && sudo chmod +x /mnt/src/etc/rc.local && sudo mkfs.ext4 -F -L uec-rootfs /dev/sdh && sudo mount /dev/sdh /mnt/target && sudo rsync -aXHAS /mnt/src/ /mnt/target && sudo umount /mnt/target && sudo umount /mnt/src"
 
 snap=$(ec2-create-snapshot ${vol} | awk {' print $2 '})
 sleep 10
 ec2-describe-snapshots ${snap}
 
 
+region=us-east-1
+arch=i386
 rel=lucid
 qurl=http://uec-images.ubuntu.com/query/lucid/server/released.current.txt
 aki=$(curl --silent "${qurl}" | awk '-F\t' '$5 == "ebs" && $6 == arch && $7 == region { print $9 }' arch=$arch region=$region )
