99d54825a045200030735fa6691634db458edfe8
inf0 <sina@anarchy.cx>
2011-07-18 10:38:54 -0700

    json for AMI tables

--- html/js/JsonToHTML/scripts/json.htmTable.js
+++ html/js/JsonToHTML/scripts/json.htmTable.js
@@ -3,42 +3,6 @@
 // objArray = Anytype of object array, like JSON results
 // theme (optional) = A css class to add to the table (e.g. <table class="<theme>">
 // enableHeader (optional) = Controls if you want to hide/show, default is show
-function CreateTableView(objArray, theme, enableHeader) {
-    // set optional theme parameter
-    if (theme === undefined) {
-        theme = 'mediumTable'; //default theme
-    }
-
-    if (enableHeader === undefined) {
-        enableHeader = true; //default enable headers
-    }
-
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    
-    var str = '<table class="' + theme + '">';
-
-    // table head
-    if (enableHeader) {
-        str += '<thead><tr>';
-        for (var index in array[0]) {
-            str += '<th scope="col">' + index + '</th>';
-        }
-        str += '</tr></thead>';
-    }
-
-    // table body
-    str += '<tbody>';
-    for (var i = 0; i < array.length; i++) {
-        str += (i % 2 == 0) ? '<tr class="alt">' : '<tr>';
-        for (var index in array[i]) {
-            str += '<td>' + array[i][index] + '</td>';
-        }
-        str += '</tr>';
-    }
-    str += '</tbody>'
-    str += '</table>';
-    return str;
-}
 
 // This function creates a details view table with column 1 as the header and column 2 as the details
 // Parameter Information
@@ -63,7 +27,7 @@ function CreateDetailView(objArray, theme, enableHeader) {
     for (var i = 0; i < array.length; i++) {
         var row = 0;
         for (var index in array[i]) {
-            str += (row % 2 == 0) ? '<tr class="alt">' : '<tr>';
+            str += (row % 2 == 0) ? '<tr class="">' : '<tr>';
 
             if (enableHeader) {
                 str += '<th scope="row">' + index + '</th>';
