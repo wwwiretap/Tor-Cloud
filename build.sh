505468a675f1fd4d395d8550b7e8d25afd202dde
root <root@inf0.(none)>
2011-09-12 17:54:01 -0400

    updated rc.local

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ relaytype=$1;
 if [ -n "$relaytype" ]; then
         echo "Starting ..."
 else
-        echo "Try ./buil.sh bridge us-east-1 for US East region."
+        echo "Try ./build.sh bridge us-east-1 for US East region."
         echo "Note: only run 1 region per build."
         echo "Obtain a list of regions using the ec2-api-tools: ec2-describe-regions"
         exit
@@ -58,7 +58,7 @@ vol=$(ec2-create-volume --size 4 --region ${region} --availability-zone ${zone} 
 ec2-attach-volume --instance ${iid} --region ${region} --device /dev/sdh ${vol}
 
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-cloud.pem ubuntu@${host} -q -t "sudo chown ubuntu:ubuntu /mnt && cd /mnt && wget http://uec-images.ubuntu.com/releases/10.04/release/ubuntu-10.04-server-uec-i386.tar.gz -O ubuntu-10.04-server-uec-i386.tar.gz && tar -Sxvzf /mnt/ubuntu-10.04-server-uec-i386.tar.gz && mkdir src target && sudo mount -o loop,rw /mnt/lucid-server-uec-i386.img /mnt/src && sudo mkfs.ext4 -F -L uec-rootfs /dev/sdh && sudo mount /dev/sdh /mnt/target"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-cloud.pem ubuntu@${host} -q -t "sudo chown ubuntu:ubuntu /mnt && cd /mnt && wget http://uec-images.ubuntu.com/releases/10.04/release/ubuntu-10.04-server-uec-i386.tar.gz -O ubuntu-10.04-server-uec-i386.tar.gz && tar -Sxvzf /mnt/ubuntu-10.04-server-uec-i386.tar.gz && sudo mkdir src target && sudo mount -o loop,rw /mnt/lucid-server-uec-i386.img /mnt/src && sudo mkfs.ext4 -F -L uec-rootfs /dev/sdh && sudo mount /dev/sdh /mnt/target"
 
 #ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-cloud.pem ubuntu@${host} -q -t "gpg --verify /mnt/SHA256SUMS.gpg /mnt/SHA256SUMS &> /mnt/verify.txt && cat /mnt/verify.txt | grep Good | awk {'print $2'})"
 
@@ -72,11 +72,9 @@ ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-c
 
 sh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-cloud.pem ubuntu@${host} -q -v -t "sudo wget https://raw.github.com/inf0/Tor-Cloud/master/rc.local -O /mnt/src/etc/rc.local"
 
-
-
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-cloud.pem ubuntu@${host} -q -v -t "sudo wget https://raw.github.com/inf0/Tor-Cloud/master/ec2-prep.sh -O /mnt/src/etc/ec2-prep.sh"
 
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-cloud.pem ubuntu@${host} -q -v -t "sudo chmod +x /mnt/src/etc/ec2-prep.sh && chmod +x /mnt/src/etc/ec2-prep.sh"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-cloud.pem ubuntu@${host} -q -v -t "sudo chmod +x /mnt/src/etc/ec2-prep.sh && sudo chmod +x /mnt/src/etc/ec2-prep.sh"
 
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-cloud.pem ubuntu@${host} -q -v -t "sudo rsync -aXHAS /mnt/src/ /mnt/target"
 
@@ -84,7 +82,7 @@ ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  -i ~/keys/tor-c
 
 
 snap=$(ec2-create-snapshot --region ${region} ${vol} | awk {' print $2 '})
-sleep 80
+sleep 111
 ec2-describe-snapshots --region ${region} ${snap}
 rel=lucid
 qurl=http://uec-images.ubuntu.com/query/lucid/server/released.current.txt
